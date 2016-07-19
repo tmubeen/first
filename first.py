b493a525ff1edd03f497b522f146516e3cc228ba
tmubeen <tauseef_mubeen2003@yahoo.com>
2016-07-18 19:51:21 -0700

    first

---
+++ first.py
@@ -0,0 +1,18 @@
+print("hello world")
+message="Hello Python World"
+print(message)
+
+message="Hello Python Crash course World"
+print(message)
+
+f_n="tauseef"
+l_n="mubeen"
+print("hello !"+f_n +l_n)
+
+a=['red','green','blue','yellow']
+print(a)
+
+a[0]='black'
+print(a)
+a.append('violet')
+print(a)
